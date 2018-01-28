d8a5e98e90207fcfeb27dd348fe9d7f33a094537
Andrew Guibert <andy.guibert@gmail.com>
2018-01-28 12:31:39 -0600

    Clean project to basic hello world example

--- src/main/java/org/aguibert/liberty/TestApp.java
+++ src/main/java/org/aguibert/liberty/TestApp.java
@@ -3,5 +3,5 @@ package org.aguibert.liberty;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("/books")
-public class BookApp extends Application { }
+@ApplicationPath("/")
+public class TestApp extends Application { }
