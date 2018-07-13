10e211b558354384e7ea1418f861c6488de5185d
Andrew Guibert <andy.guibert@gmail.com>
2018-07-13 14:47:37 -0500

    Sample using H2 Database with JPA

--- src/main/java/org/aguibert/liberty/Book.java
+++ src/main/java/org/aguibert/liberty/Book.java
@@ -6,17 +6,26 @@ import javax.persistence.Id;
 
 @Entity
 public class Book {
-	
-	@Id
-	public int id;
-	
-	@Basic
-	public String author;
-	
-	@Basic
-	public String title;
-	
-	@Basic
-	public int pages;
+
+    @Id
+    public int id;
+
+    @Basic
+    public String author;
+
+    @Basic
+    public String title;
+
+    @Basic
+    public int pages;
+
+    public Book() {}
+
+    public Book(int id, String author, String title, int pages) {
+        this.id = id;
+        this.author = author;
+        this.title = title;
+        this.pages = pages;
+    }
 
 }
--- src/main/java/org/aguibert/liberty/TestService.java
+++ src/main/java/org/aguibert/liberty/TestService.java
@@ -3,13 +3,27 @@ package org.aguibert.liberty;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 
+import javax.annotation.Resource;
 import javax.enterprise.context.ApplicationScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.sql.DataSource;
+import javax.transaction.UserTransaction;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 @Path("/")
 @ApplicationScoped
 public class TestService {
+
+    @Resource(lookup = "jdbc/h2test")
+    DataSource ds;
+
+    @PersistenceContext(unitName = "h2test")
+    EntityManager em;
+
+    @Resource
+    UserTransaction tx;
 
     @GET
     public String test() {
@@ -29,13 +43,21 @@ public class TestService {
 
     private void doTest() throws Exception {
         log("Hello world");
+        ds.getConnection().close();
+        log("Got a connection");
+
+        tx.begin();
+        em.persist(new Book(1, "Jon", "Some Book", 10));
+        Book found = em.find(Book.class, 1);
+        log("Found book: " + found);
+        tx.commit();
     }
 
     private StringWriter sb = new StringWriter();
 
     private void log(String msg) {
         System.out.println(msg);
-        sb.append(msg);
+        sb.append(msg.replace("\n", "<br>"));
         sb.append("<br/>");
     }
 
