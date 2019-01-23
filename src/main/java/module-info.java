9db505b2f5cfe8da7487b664bf6f7195ccd6273f
Andrew Guibert <andy.guibert@gmail.com>
2019-01-23 16:47:20 -0600

    Java 11 blog post cleanup

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-open module org.aguibert.liberty.basic_app {
-
-    requires java.base;
-    requires java.sql;
-
-    requires java.ws.rs;
-
-}
--- src/main/java/org/aguibert/liberty/TestService.java
+++ src/main/java/org/aguibert/liberty/TestService.java
@@ -1,35 +1,20 @@
 package org.aguibert.liberty;
 
-import java.io.PrintWriter;
 import java.io.StringWriter;
 
-import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 
 @Path("/")
-@ApplicationScoped
+@RequestScoped
 public class TestService {
 
     @GET
     public String test() {
-        try {
-            log(">>> ENTER");
-            doTest();
-            log("<<< EXIT SUCCESSFUL");
-        } catch (Exception e) {
-            e.printStackTrace(System.out);
-            e.printStackTrace(new PrintWriter(sb));
-            log("<<< EXIT FAILED");
-        }
-        String result = sb.toString();
-        sb = new StringWriter();
-        return result;
-    }
-
-    private void doTest() throws Exception {
         log("Hello world");
-        log("This class is in: " + getClass().getModule());
+        log("The java runtime is version:" + System.getProperty("java.specification.version"));
+        return sb.toString();
     }
 
     private StringWriter sb = new StringWriter();
